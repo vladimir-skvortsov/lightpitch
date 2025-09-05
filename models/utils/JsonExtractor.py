eb3f7c215ba27db1c8d9c501c16a3bf7cc34463e
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2025-09-05 23:31:37 +0300

    feat: add JsonExtractor

---
+++ models/utils/JsonExtractor.py
@@ -0,0 +1,14 @@
+import re
+from langchain_core.runnables import Runnable
+
+
+class JsonExtractor(Runnable):
+    json_pattern = r'\{.*?\}'
+
+    def invoke(self, input_data: str, *args) -> str:
+        matches = re.findall(JsonExtractor.json_pattern, input_data, re.DOTALL)
+
+        if matches:
+            return matches[-1].strip().replace('\\\\', '\\')
+
+        return input_data
